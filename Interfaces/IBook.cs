172c9ae054db4551ea904d9929cbb888f93d8fe8
Raviv <raken100@gmail.com>
2024-10-05 16:35:55 +0300

    Added the content of the IBook interface

--- Interfaces/IBook.cs
+++ Interfaces/IBook.cs
@@ -2,7 +2,22 @@ using System;
 
 namespace OOPProject.Interfaces;
 
+/// <summary>
+/// Interface for a book, for using abstraction principle
+/// </summary>
 public interface IBook
 {
+    #region Properties
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int PagesAmount { get; set; }
+    public DateTime PublishDate { get; set; }
+    public bool IsAvailable { get; set; }
 
+    #endregion
+    #region Methods
+    public void Borrow();
+    public void Return();
+    public void ReadDescription();
+    #endregion
 }
