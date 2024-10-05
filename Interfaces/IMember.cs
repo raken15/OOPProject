0f3cf1ac685ed369210d6ef5f2b15832d4fd1f27
Raviv <raken100@gmail.com>
2024-10-05 18:24:26 +0300

    Fully written the content for the interfaces

--- Interfaces/IMember.cs
+++ Interfaces/IMember.cs
@@ -1,4 +1,5 @@
 using System;
+using OOPProject.Classes;
 
 namespace OOPProject.Interfaces;
 
@@ -7,5 +8,17 @@ namespace OOPProject.Interfaces;
 /// </summary>
 public interface IMember
 {
-
+    #region Properties
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public List<Book> BorrowedBooks { get; set; }
+    public string Role { get; set; }
+    #endregion
+    #region Methods
+    public void BorrowBook(Book book);
+    public void ReturnBook(Book book);
+    public void DisplayMember();
+    #endregion
 }
