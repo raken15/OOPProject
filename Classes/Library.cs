3e56d678789e7dac8107f21d7f94f9b06a228b5d
Raviv <raken100@gmail.com>
2024-10-06 08:42:36 +0300

    Added implementation to library class

--- Classes/Library.cs
+++ Classes/Library.cs
@@ -1,4 +1,5 @@
 using System;
+using OOPProject.Interfaces;
 
 namespace OOPProject.Classes;
 
@@ -7,5 +8,41 @@ namespace OOPProject.Classes;
 /// </summary>
 public class Library
 {
+    public List<IBook> Books { get; set; }
+    public List<IMember> Members { get; set; }
+
+    public Library()
+    {
+        Books = new List<IBook>();
+        Members = new List<IMember>();
+    }
+    public void AddBook(IBook iBook)
+    {
+        if (Books.Contains(iBook))
+        {
+            Console.WriteLine($"{iBook.Title} is already in the library.");
+            return;
+        }
+        
+        Books.Add(iBook);
+        Console.WriteLine($"{iBook.Title} added to the library.");
+    }
+    public void RegisterMember(IMember iMember)
+    {
+        if (Members.Contains(iMember))
+        {
+            Console.WriteLine($"{iMember.Name} is already a library member.");
+            return;
+        }
+        Members.Add(iMember);
+        Console.WriteLine($"{iMember.Name} registered as a library member.");
+    }
+    public void DisplayAvailableBooks()
+    {
+        foreach (var book in Books.Where(b => b.IsAvailable))
+        {
+            Console.WriteLine(book);
+        }
+    }
 
 }
